8aebb6b12bac058f4e78358d6336822780ab08d2
Peter Jensen <petetjensen@gmail.com>
2017-02-27 18:06:33 -0700

    first

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const fsp = require('fs-promise');
+const path = require("path");
+
+	module.exports.resolvedPath = function(directoryPath, fileName) {
+		return path.resolve(directoryPath, fileName);
+	}
+
+	function readFile(path){
+		return new Promise(function(resolve, reject){
+			fs.readFile(path, (err, data) => {
+			  if (err){
+			  	reject(err);
+			  }
+			  else{
+			  	console.log(data);
+			  	resolve(data);
+			  }
+			  
+			});
+		})
+	}
+	module.exports.readDir= function(path){
+		return new Promise(function(resolve, reject){
+			fs.readdir(path, (err, data) => {
+			  if (err){
+			  	reject(err);
+			  }
+			  else{
+			  	console.log(data);
+			  	resolve(data);
+			  }
+			  
+			});
+		})
+	}
+
+	module.exports.readDirFiles = function(path){
+		var finalProduct = []
+		return module.exports.readDir(path)
+		.then(function(directory){
+			directory.forEach(function(file){
+				finalProduct.push(module.exports.readFile(module.exports.resolvedPath(path, file)));
+			})
+			return Promise.all(finalProduct);
+		})
+	}
+	
